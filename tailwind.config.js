04fa3d113694ed1c35d5006f2c8015863c188408
lizalkv23 <112826952+lizalkv23@users.noreply.github.com>
2023-03-02 02:01:48 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      lg: { 'max': "992px" },
+      md: { 'max': "768px" },
+      sm: { 'max': "480px" },
+    },
+    extend: {},
+  },
+  plugins: [],
+}
